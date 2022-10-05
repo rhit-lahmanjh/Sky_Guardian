feea325a8d36eda1541a4216a6e741f5708c29fd
Joseph Lahmann <lahmanjh@rose-hulman.edu>
2022-10-05 14:40:48 -0400

    fixed displaying video in extended Qwidget

--- main/gui_suggestion.py
+++ main/gui_suggestion.py
@@ -5,34 +5,44 @@ from PyQt5 import QtGui
 import djitellopy as tel
 from threading import Thread
 import cv2
-import time
+import time as t
 import sys
 import numpy as np
 
 class ourTello(tel.Tello):
+    vidCap = []
     def __init__(self,name):
         super().__init__()
         self.name = name
+        self.connect()
+        self.streamon()
+        self.vidCap = self.get_video_capture()
+
+    def getFrame(self):
+        return self.vidCap.read()
+
+    def stop(self):
+        self.vidCap.release()
+        self.streamoff()
+        self.end()
+
     
 class VideoThread(QThread):
     change_pixmap_signal = pyqtSignal(np.ndarray)
     drone = []
-    frameObj = []
-    frame = []
 
     def __init__(self,drone):
         super().__init__()
         self._run_flag = True
         self.drone = drone
-        self.frameObj = drone.get_frame_read()
 
     def run(self):
         while self._run_flag:
-            self.frame = self.frameObj.frame # might not be necessary, i'm not sure how the background frame object stores and updates frame
-            goo = self.frameObj.frame
-            self.change_pixmap_signal.emit(self.frame)
-        # shut down capture system
-
+            returned, cv_img = self.drone.getFrame()
+            if returned:
+                self.change_pixmap_signal.emit(cv_img)
+    
+    # shut down capture system
     def stop(self):
         """Sets run flag to False and waits for thread to finish"""
         self._run_flag = False
@@ -40,13 +50,13 @@ class VideoThread(QThread):
 
 class vidFeed(QWidget):
     drone = []
+    image_label = []
 
     #constructor that takes in an individual drone, extended in the class above
     def __init__(self,drone,layout):
         super().__init__()
         self.setWindowTitle('')
         self.drone = drone
-        self.drone.streamon()
         self.display_width = 720
         self.display_height = 960
         self.textLabel = QLabel(drone.name)
@@ -57,7 +67,6 @@ class vidFeed(QWidget):
 
         #assign to space in layout
         layout.addWidget(self.image_label,0,0)
-        # layout.addWidget(self.textLabel)
 
         #create Thread
         self.thread = VideoThread(drone)
@@ -65,8 +74,8 @@ class vidFeed(QWidget):
         self.thread.start()
 
     @pyqtSlot(np.ndarray)
-    def update_image(self,rgb_img):
-        qt_img = self.convert_rgb_qt(rgb_img)
+    def update_image(self,cv_img):
+        qt_img = self.convert_cv_qt(cv_img)
         self.image_label.setPixmap(qt_img)
 
     def convert_cv_qt(self,cv_img):
@@ -87,30 +96,15 @@ class vidFeed(QWidget):
     def closeEvent(self, event):
         self.thread.stop()
         event.accept()
-        chuck.streamoff()
-        chuck.end()
+        self.drone.stop()
 
 # initial connection
 chuck = ourTello('Chuck')
-# chuck.connect_to_wifi('TELLO-F28715','12345678')
-chuck.connect()
-chuck.streamon()
-# chuck.set_wifi_credentials('TELLO-F28715', 'chuck')
-# chuck.end()
-
-frameGetter = chuck.get_frame_read()
-# frame = frameGetter.frame
-print('Here')
 
 app = QApplication([])
 cam_layout = QGridLayout()
 win = vidFeed(chuck, cam_layout)
 win.setGeometry(200,200,720,960)
-
 win.setWindowTitle("Sky Guardian")
-chuck.get_udp_video_address()
-
-win.setLayout(cam_layout)
-
 win.show()
 app.exec()
