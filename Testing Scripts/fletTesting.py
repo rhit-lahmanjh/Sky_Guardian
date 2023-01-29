a5c4d502a57630750ecdda45a94ff25de110249a
Kirk Preston <45315167+prestokp@users.noreply.github.com>
2023-01-29 13:19:10 -0500

    Routing Code Practice

--- Testing Scripts/fletTesting.py
+++ Testing Scripts/fletTesting.py
@@ -1,6 +1,8 @@
 import flet as ft
 import time
 
+import flet as ft
+
 def main(page: ft.Page):
     page.title = "Routes Example"
 
@@ -36,6 +38,20 @@ def main(page: ft.Page):
     page.on_view_pop = view_pop
     page.go(page.route)
 
+
+ft.app(target=main, view=ft.WEB_BROWSER)
+
+# Page Reference Error
+"""
+def view_pop(view):
+    page.views.pop()
+    top_view = page.views[-1]
+    page.go(top_view.route)
+
+    page.on_route_change = route_change
+    page.on_view_pop = view_pop
+    page.go(page.route)
+"""
 
 ft.app(target=main)
 
