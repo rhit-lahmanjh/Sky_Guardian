de66c44583dd168c4f79f34ea4bbff5b677f90ad
Kirk Preston <45315167+prestokp@users.noreply.github.com>
2023-05-16 17:13:39 -0400

    Row Addition Progress

--- main/connectivity_page.py
+++ main/connectivity_page.py
@@ -83,77 +83,8 @@ def main(page: ft.Page):
 
     def droneTwoConnect_button(e):
 
-        print("Connecting to Drone 2")
-        # Input appropriate FSM drone logic
-        # User input should be the string IP address
-        userIPaddress2 = str(input2.value)
+        print("Storing Drone 2 IP Address")
         device_data.update({'DRONE2_IP':input2.value})
-        # portValue = 8889
-
-        # print("Address assigned")
-        # tello2_address = (userIPaddress2, portValue)
-        # # Create a UDP connection that we'll send the command to
-        # sock2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        # local_address = ('192.168.0.234', 9010)
-
-        # # Let's be explicit and bind to a local port on our machine where Tello can send messages
-        # print("Sock Binded")
-        # sock2.bind(local_address)
-
-        # def send(message):
-        #     try:
-        #         sock2.sendto(message.encode(), tello2_address)
-        #         print("Sending message: " + message)
-        #     except Exception as e:
-        #         print("Error sending: " + str(e))
-
-        # # Function that listens for messages from Tello and prints them to the screen
-        # def receive():
-        #     while True:
-        #         try:
-        #             response2, ip_address = sock2.recvfrom(128)
-        #             print("Received message from Tello Two: " + response2.decode(encoding='utf-8'))
-        #         except Exception as e:
-        #             sock2.close()
-        #             print("Error receiving: " + str(e))
-        #             break
-
-        # print("daemon Thread")
-        # receiveThread = threading.Thread(target=receive)
-        # receiveThread.daemon = True
-        # receiveThread.start()
-
-        # # Send Tello into command mode
-        # print("Send Function")
-        # send("command")
-        # # Receive response from Tello
-        # print("Receive Function")
-        # receive()
-        # # Delay 3 seconds before we send the next command
-        # print("Sleep")
-        # time.sleep(3)
-        # # Ask Tello about battery status
-        # print("Battery Status")
-        # send("battery?")
-        # # Receive battery response from Tello
-        # print("Awaiting Reception")
-        # receive()
-        # print("Response Received")
-
-        # print("Successfully Connected to Drone")
-        # print("Closing socket")
-        # sock2.close()
-        # SuccessfulConnection = True
-        # if SuccessfulConnection:
-        #     page.add("Drone 2 Connection Successful")
-        #     page.add(droneContinueButtonRow)
-        # print("Successfully Connected to Drone")
-        # print("Closing socket")
-        # sock2.close()
-        # SuccessfulConnection = True
-        # if SuccessfulConnection:
-        #     page.add("Drone 2 Connection Successful")
-        # page.update()
 
 
     def window_event(e):
@@ -172,9 +103,6 @@ def main(page: ft.Page):
     def no_click(e):
         confirm_dialog.open = False
         page.update()
-
-    def continueButton(e):
-        print("Routing to Dashboard")
 
     input1 = ft.TextField(label="Enter New IP Address", on_submit=droneOneConnect_button)
 
@@ -214,16 +142,40 @@ def main(page: ft.Page):
         ]
     )
 
-    droneContinueButtonItems = [
-        ft.ElevatedButton(text="Continue",color=ft.colors.BLACK, on_click=continueButton, bgcolor=ft.colors.GREEN_200)
+    ssidInput = ft.TextField(label="Enter Router SSID Name", on_submit=ssidSaveButton)
+
+    ssidConnectionItems = [
+        # using input from SSID text field
+        ssidInput,
+        ft.ElevatedButton(text="Save",color=ft.colors.BLACK, on_click=ssidSaveButton, bgcolor=ft.colors.AMBER)
     ]
 
-    droneContinueButtonRow = ft.Row(
+    ssidConnectionRow = ft.Row(
         [
+            ft.Text(value="SSID", style=ft.TextThemeStyle.DISPLAY_SMALL, text_align=ft.TextAlign.CENTER),
             ft.Container(
                 content=ft.Row(
-                    droneContinueButtonItems,
-                    alignment=ft.MainAxisAlignment.END),
+                    ssidConnectionItems,
+                    alignment=ft.MainAxisAlignment.CENTER),
+            )
+        ]
+    )
+
+    routerPasswordInput = ft.TextField(label="Enter Router Password", on_submit=passwordSaveButton)
+
+    routerPasswordConnectionItems = [
+        # using input from router password text field
+        routerPasswordInput,
+        ft.ElevatedButton(text="Save", color=ft.colors.BLACK, on_click=passwordSaveButton, bgcolor=ft.colors.AMBER)
+    ]
+
+    routerPasswordConnectionRow = ft.Row(
+        [
+            ft.Text(value="Password", style=ft.TextThemeStyle.DISPLAY_SMALL, text_align=ft.TextAlign.CENTER),
+            ft.Container(
+                content=ft.Row(
+                    routerPasswordConnectionItems,
+                    alignment=ft.MainAxisAlignment.CENTER),
             )
         ]
     )
@@ -233,7 +185,8 @@ def main(page: ft.Page):
         [
             droneOneConnectionRow,
             droneTwoConnectionRow,
-            droneContinueButtonRow
+            ssidConnectionRow,
+            routerPasswordConnectionRow
         ],
         spacing=50,
         alignment=ft.MainAxisAlignment.CENTER,
@@ -251,12 +204,12 @@ def main(page: ft.Page):
         actions_alignment=ft.MainAxisAlignment.END,
     )
 
+    page.update()
     dr1 = ft.Text(value=f"Current Drone 1 Router IP: {device_data.get('DRONE1_IP')}", color="white",scale=1,weight=4)
     page.controls.append(dr1)
+    page.update()
     dr2 = ft.Text(value=f"Current Drone 2 Router IP: {device_data.get('DRONE2_IP')}", color="white",scale=1,weight=4)
     page.controls.append(dr2)
-    # ds = ft.Text(value="Drone Single IP: 192.168.10.1", color="black")
-    # page.controls.append(ds)
     page.update()
 
 ft.app(target=main)
